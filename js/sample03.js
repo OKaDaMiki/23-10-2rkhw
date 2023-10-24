f13a4d0e28282a967502f0eefc5083169def9d79
OkdMk <138852873+OkdMk@users.noreply.github.com>
2023-10-24 15:27:59 +0900

    完了分
    sample03が完成した

--- js/sample03.js
+++ js/sample03.js
@@ -1,5 +1,9 @@
-document .querySelector(".btn") .addEventListener(
-    "click" ,function(){
-        console.log(document .querySelector(".tebox") .value)
-    }
-);
+const btn = document.querySelector(".btn");
+const hyokkori = document.querySelector(".hyokkori");
+const tebox = document.querySelector(".tebox");
+
+btn.addEventListener("click", function () {
+    // console.log(document.querySelector(".tebox").value);
+    // console.dir(document .querySelector(".hyokkori") );
+    hyokkori.textContent = tebox.value;
+});
