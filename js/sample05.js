8cebe0b90e73de3eaa9a72f5fb67b1fa5e3e02cf
OkdMk <138852873+OkdMk@users.noreply.github.com>
2023-11-06 23:42:04 +0900

    迷走中
    sample05が進んでいるか戻っているかわからない。

--- js/sample05.js
+++ js/sample05.js
@@ -1,15 +1,17 @@
 //四角の上でマウスを押した時、四角の真ん中の座標をマウスの座標にする
 //マウスのボタンが上がった時にマウスの座標の補足を外す
 const square = document.querySelector(".square");
-const sWidth = square.style.clientwidth;
-const sHeight = square.style.clientheight;
-const sTop = square.style.clienttop;
-const sLeft = square.style.clientleft;
-// const left =
-// const squareCenterLeft = left + width / 2;
-// const squareCenterTop = top + height / 2;
-
-// addEventListener("mousedown", (event) => {
-// });
+// let sWidth = square.style.clientwidth;
+// let sHeight = square.style.clientheight;
+// let sTop = square.style.clienttop;
+// let sLeft = square.style.clientleft;
 
 console.dir(square);
+
+// document.addEventListener("mousedown", function () {
+document.addEventListener("mousemove", function (event) {
+    square.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+    // square.style.top = event.clientX;
+    // square.style.left = event.clientY;
+});
+// });
